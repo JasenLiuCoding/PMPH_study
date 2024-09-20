de1b40ed9b77f12c81eb3645538078979c190bc4
JasenLiuCoding <410952788@qq.com>
2024-09-20 15:18:29 +0200

    bug fix

--- week2/w2-code-handin/cuda/pbb_kernels.cuh
+++ week2/w2-code-handin/cuda/pbb_kernels.cuh
@@ -196,7 +196,7 @@ class Mssp {
 template<class OP>
 __device__ inline typename OP::RedElTp
 scanIncWarp( volatile typename OP::RedElTp* ptr, const unsigned int idx ) {
-    const unsigned int lane = idx & (WARP-1);x
+    const unsigned int lane = idx & (WARP-1);
     const unsigned int k = __log2f(WARP);
     if(lane==0) {
 

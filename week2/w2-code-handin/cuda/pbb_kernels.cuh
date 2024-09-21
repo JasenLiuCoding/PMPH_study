6d3ef5b36cd773fecfadb018864cb818aa8c39d5
JasenLiuCoding <410952788@qq.com>
2024-09-21 16:23:17 +0200

    debug

--- week2/w2-code-handin/cuda/pbb_kernels.cuh
+++ week2/w2-code-handin/cuda/pbb_kernels.cuh
@@ -498,9 +498,9 @@ copyFromShr2GlbMem( const uint32_t glb_offs
                   , volatile T* shmem_red
 ) {
     #pragma unroll
-    for (uint32_t i = 0; i < CHUNK; i++) {
-        uint32_t loc_ind = threadIdx.x * CHUNK + i;
-        // uint32_t loc_ind = threadIdx.x + (CHUNK)*i;
+    for (uint32_t i = 0; i < blockDim.x; i++) {
+        // uint32_t loc_ind = threadIdx.x * CHUNK + i;
+        uint32_t loc_ind = threadIdx.x + CHUNK*i;
         uint32_t glb_ind = glb_offs + loc_ind;
         if (glb_ind < N) {
             T elm = const_cast<const T&>(shmem_red[loc_ind]);

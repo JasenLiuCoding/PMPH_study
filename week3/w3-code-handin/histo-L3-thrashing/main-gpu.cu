43d446c57b757a69648321f7dcb03eb54795d7f5
JasenLiuCoding <410952788@qq.com>
2024-09-27 15:05:29 +0200

    update

--- week3/w3-code-handin/histo-L3-thrashing/main-gpu.cu
+++ week3/w3-code-handin/histo-L3-thrashing/main-gpu.cu
@@ -41,15 +41,15 @@ void multiStepHisto ( uint32_t* d_inp_inds
      * but is essentially equivalent with the naive kernel (hence
      * it has roughly the same performance).
      ****************************************************************/
-    {
+    // {
         
-        multiStepKernel<<<grid,B>>>(d_inp_inds, d_inp_vals, d_hist, N, 0, H);
-    }
-    // for (uint32_t k=0; k<num_partitions; k++){
-    //     uint32_t LB = k*CHUNK;
-    //     uint32_t UB = min( (k+1)*CHUNK, H );
-    //     multiStepKernel<<<grid,B>>>(d_inp_inds, d_inp_vals, d_hist, N, LB, UB);
+    //     multiStepKernel<<<grid,B>>>(d_inp_inds, d_inp_vals, d_hist, N, 0, H);
     // }
+    for (uint32_t k=0; k<num_partitions; k++){
+        uint32_t LB = k*CHUNK;
+        uint32_t UB = min( (k+1)*CHUNK, H );
+        multiStepKernel<<<grid,B>>>(d_inp_inds, d_inp_vals, d_hist, N, LB, UB);
+    }
 }
 
 /**

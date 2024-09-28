d097d47030d1aff021b1e807548c040f7c9a0a26
JasenLiuCoding <410952788@qq.com>
2024-09-28 19:44:37 +0200

    debug

--- week3/w3-code-handin/gpu-coalescing/kernels.cu.h
+++ week3/w3-code-handin/gpu-coalescing/kernels.cu.h
@@ -96,13 +96,13 @@ template<class ElTp>
 __global__ void 
 transKernel(ElTp* A_tr, ElTp* B_tr, uint32_t num_rows, uint32_t num_cols) {
     uint32_t gid = blockIdx.x * blockDim.x + threadIdx.x;
-    if(gid >= num_rows) return;
+    if(gid >= num_cols) return;
 
     ElTp accum = 0;
-    for(int j=0; j<num_cols; j++) {
-        ElTp el_a  = A_tr[ j*num_cols + gid ];
+    for(int j=0; j<num_rows; j++) {
+        ElTp el_a  = A_tr[ gid*num_rows + j ];
         accum = sqrt(accum) + el_a * el_a;
-        B_tr[ j*num_cols + gid ] = accum;
+        B_tr[ gid*num_rows + j ] = accum;
     }
 }
 
